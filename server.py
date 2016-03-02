86749a47f28b35977b1d444750ced337c836412e
diogo <a233894432@gmail.com>
2016-03-02 14:59:40 +0800

    1\ 更新

--- server.py
+++ server.py
@@ -16,7 +16,6 @@ def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
-
     print("Development server is running at http://127.0.0.1:%s" % options.port)
     print("Quit the server with Control-C")
 
